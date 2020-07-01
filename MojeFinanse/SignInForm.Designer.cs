4d2177c2940e5d01b774b0fe7a2c5d907cfb71d1
Mateusz Szymański <smokus990@gmail.com>
2020-07-01 20:01:17 +0200

    Przygotowanie podstawowych przycisków logowania

--- MojeFinanse/SignInForm.Designer.cs
+++ MojeFinanse/SignInForm.Designer.cs
@@ -28,20 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordBox = new System.Windows.Forms.TextBox();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.signinButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form1
+            // loginBox
+            // 
+            this.loginBox.Location = new System.Drawing.Point(107, 44);
+            this.loginBox.Name = "loginBox";
+            this.loginBox.Size = new System.Drawing.Size(145, 20);
+            this.loginBox.TabIndex = 0;
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Location = new System.Drawing.Point(22, 47);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(36, 13);
+            this.loginLabel.TabIndex = 1;
+            this.loginLabel.Text = "Login:";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(22, 87);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(39, 13);
+            this.passwordLabel.TabIndex = 3;
+            this.passwordLabel.Text = "Hasło:";
+            // 
+            // passwordBox
+            // 
+            this.passwordBox.Location = new System.Drawing.Point(107, 84);
+            this.passwordBox.Name = "passwordBox";
+            this.passwordBox.Size = new System.Drawing.Size(145, 20);
+            this.passwordBox.TabIndex = 2;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(25, 143);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(93, 30);
+            this.closeButton.TabIndex = 4;
+            this.closeButton.Text = "Zamknij";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // signinButton
+            // 
+            this.signinButton.Location = new System.Drawing.Point(159, 143);
+            this.signinButton.Name = "signinButton";
+            this.signinButton.Size = new System.Drawing.Size(93, 30);
+            this.signinButton.TabIndex = 5;
+            this.signinButton.Text = "Zaloguj";
+            this.signinButton.UseVisualStyleBackColor = true;
+            this.signinButton.Click += new System.EventHandler(this.signinButton_Click);
+            // 
+            // SignInForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(279, 243);
+            this.Controls.Add(this.signinButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.passwordBox);
+            this.Controls.Add(this.loginLabel);
+            this.Controls.Add(this.loginBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "SignInForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logowanie";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SignInForm_FormClosing);
+            this.Load += new System.EventHandler(this.SignInForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox loginBox;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.TextBox passwordBox;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Button signinButton;
     }
 }
 
--- MojeFinanse/SignInForm.cs
+++ MojeFinanse/SignInForm.cs
@@ -17,5 +17,36 @@ namespace MojeFinanse
             InitializeComponent();
         }
 
+        private void SignInForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+                Close();
+        }
+
+        private void signinButton_Click(object sender, EventArgs e)
+        {
+
+            if (this.loginBox.Text == "abc" && this.passwordBox.Text == "cda")
+            {
+                //Zaloguj
+            }
+            else
+            {
+                MessageBox.Show("Nieprawidłowe hasło lub login!", "Błąd!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SignInForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show("Czy napewno chcesz zamknąć?", "Zamykanie", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
     }
 }
